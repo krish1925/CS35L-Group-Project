611985708ac4e7a1298abda7da4977a35c299865
aylahch <aylahch02@gmail.com>
2023-03-12 04:27:50 -0700

    signup and login work, but it will not let you pass the login bc the dashboard cant access info that isnt put in backend

--- fit-match/client/src/components/AuthModal.js
+++ fit-match/client/src/components/AuthModal.js
@@ -1,79 +1,89 @@
-import {useState} from 'react'
-import {useCookies} from 'react-cookie'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-function AuthModal({setShowModal, isSignUp}) {
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-    const [email, setEmail] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [confirmPassword, setConfirmPassword] = useState(null);
-    const [error, setError] = useState(null);
-    const [cookies, setCookie, removeCookie] = useCookies(['user'])
-    console.log(email, password, confirmPassword);
+function AuthModal({ setShowModal, isSignUp }) {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [error, setError] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(['user']);
+  const navigate = useNavigate(); // define navigate using useNavigate hook
+  console.log(email, password, confirmPassword);
 
-    function handleClick() {
-        setShowModal(false);
+  function handleClick() {
+    setShowModal(false);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (isSignUp && password !== confirmPassword) {
+        setError('Passwords need to match!');
+      }
+      console.log('make a post request to our database');
+      const response = await axios.post(
+        `http://localhost:8000/${isSignUp ? 'signup' : 'login'}`,
+        { email, password }
+      );
+      setCookie('Email', response.data.email, { path: '/' });
+      setCookie('userId', response.data.userId, { path: '/' });
+      setCookie('AuthToken', response.data.token, { path: '/' });
+
+      const success = response.status === 201;
+
+      if (success && isSignUp) navigate('/onboarding'); // use navigate here
+      if (success && !isSignUp) navigate('/dashboard');
+
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try{
-            if(isSignUp && (password !== confirmPassword)) {
-                setError('Passwords need to match!');
-            }
-            console.log('make a post request to our database');
-            const response = await axios.post(`http://localhost:8000/${isSignUp ? 'signup' : 'login'}`, { email, password})
-            setCookie('Email', response.data.email, { path: '/' })
-            setCookie('userId', response.data.userId, { path: '/' })
-            setCookie('AuthToken', response.data.token, { path: '/' })
-            
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  return (
+    <div className='auth-modal'>
+      <div className='close-icon' onClick={handleClick}>
+        Ⓧ
+      </div>
+      <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
 
-    
+      <form onSubmit={handleSubmit}>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          placeholder='email'
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          id='password'
+          name='password'
+          placeholder='password'
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {isSignUp && (
+          <input
+            type='password'
+            id='password-check'
+            name='password-check'
+            placeholder='confirm password'
+            required={true}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
+        <input className='secondary-button' type='submit' />
+        <p>{error}</p>
+      </form>
 
-    return (
-        <div className="auth-modal">
-            <div className="close-icon" onClick={handleClick}>Ⓧ</div>
-            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
-            
-
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="email"
-                    required={true}
-                    onChange={(e) => setEmail(e.target.value)}
-                />   
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="password"
-                    required={true}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                {isSignUp && <input
-                    type="password"
-                    id="password-check"
-                    name="password-check"
-                    placeholder="confirm password"
-                    required={true}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />}
-                <input className="secondary-button" type="submit"/>
-                <p>{error}</p>
-
-            </form>
-
-            <hr/>
-            <h2>You don't have to do it alone. Can change this later...</h2>
-        </div>
-    );
+      <hr />
+      <h2>You don't have to do it alone. Can change this later...</h2>
+    </div>
+  );
 }
 
 export default AuthModal;
--- fit-match/server/index.js
+++ fit-match/server/index.js
@@ -51,6 +51,38 @@ app.post('/signup', async (req, res) => {
     }
 })
 
+
+app.post('/login', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {email, password} = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({email})
+
+        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+        if (user && correctPassword) {
+            const token = jwt.sign(user, email, {
+                expiresIn: 60 * 24
+            })
+            res.status(201).json({token, userId: user.user_id, email})
+        }
+
+        res.status(400).send('Invalid Credentials')
+
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.close()
+    }
+})
+
+
+
 app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
     try {
@@ -65,4 +97,59 @@ app.get('/users', async (req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
+app.get('/user', async (req, res) => {
+    const client = new MongoClient(uri)
+    const userId = req.query.userId
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: userId}
+        const user = await users.findOne(query)
+        res.send(user)
+
+    } finally {
+        await client.close()
+    }
+})
+app.put('/user', async (req, res) => {
+    const client = new MongoClient(uri)
+    const formData = req.body.formData
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: formData.user_id}
+
+        const updateDocument = {
+            $set: {
+                first_name: formData.first_name,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                show_gender: formData.show_gender,
+                gender_identity: formData.gender_identity,
+                gender_interest: formData.gender_interest,
+                url: formData.url,
+                about: formData.about,
+                matches: formData.matches
+            },
+        }
+
+        const insertedUser = await users.updateOne(query, updateDocument)
+
+        res.json(insertedUser)
+
+    } finally {
+        await client.close()
+    }
+})
+
+
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT)) 
+
+
