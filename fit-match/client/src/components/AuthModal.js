7f3f8bb6e60981502d6e350bf9c5e3d30359b512
Sam Hopkins <samthehopkin@gmail.com>
2023-03-11 20:34:45 -0800

    begin authmodal implementation

--- fit-match/client/src/components/AuthModal.js
+++ fit-match/client/src/components/AuthModal.js
@@ -1,4 +1,6 @@
 import {useState} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function AuthModal({setShowModal, isSignUp}) {
 
@@ -7,19 +9,26 @@ function AuthModal({setShowModal, isSignUp}) {
     const [confirmPassword, setConfirmPassword] = useState(null);
     const [error, setError] = useState(null);
 
+    let navigate = useNavigate()
     console.log(email, password, confirmPassword);
 
     function handleClick() {
-        setShowModal(false);
+        setShowModal(false)
     }
 
-    function handleSubmit(e) {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try{
             if(isSignUp && (password !== confirmPassword)) {
-                setError('Passwords need to match!');
+                setError('Passwords need to match!')
+                return
             }
-            console.log('make a post request to our database');
+
+            const response = await axios.post('http://localhost:8000/signup', {email, password})
+            const success = response.status === 201
+
+            if (success) navigate ('/onboarding')
+
         } catch (error) {
             console.log(error);
         }
--- fit-match/server/index.js
+++ fit-match/server/index.js
@@ -1,16 +1,53 @@
 const PORT = 8000
 const express = require('express')
 const { MongoClient } = require('mongodb')
+const {v4: uuidv4} = require('uuid')
+const jwt = require('jsonwebtoken')
 const uri = "mongodb+srv://project:project@35lproject.3p5ldxo.mongodb.net/?retryWrites=true&w=majority"
-
+const cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json('hello')
 }) 
 
-app.post('/signup', (req, res) => {
-    res.json('Hello to my app')
+app.post('/signup', async (req, res) => {
+    const client = new MongoClient(uri)
+    console.log(req.body)
+    const { email, password} = req.body
+
+    const generatedUserId = uuidv4()
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    try {
+        client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const existingUser = users.findOne({ email })
+
+        if (existingUser) {
+            return res.status(409).send('User already exists. Please login')
+        }
+
+        const sanitizedEmail = email.toLowerCase()
+
+        const data = {
+            user_id: generatedUserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword
+        }
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn: 60 * 24,
+        })
+        res.status(201).json({ token, userId: generatedUserId, sanitizedEmail})
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 app.get('/users', async (req, res) => {
