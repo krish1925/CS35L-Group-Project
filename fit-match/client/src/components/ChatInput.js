bfcac3a6d6332905f4d8bf09ffc321d9e642c304
Sam Hopkins <samthehopkin@gmail.com>
2023-03-13 18:47:41 -0700

    implement ChatInput

--- fit-match/client/src/components/ChatInput.js
+++ fit-match/client/src/components/ChatInput.js
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useState} from 'react'
+import axios from 'axios'
 
-function ChatInput() {
-    const[textArea, setTextArea] = useState(null);
+const ChatInput = ({ user, clickedUser, getUserMessages, getClickedUsersMessages }) => {
+    const [textArea, setTextArea] = useState("")
+    const userId = user?.user_id
+    const clickedUserId = clickedUser?.user_id
+
+    const addMessage = async () => {
+        const message = {
+            timestamp: new Date().toISOString(),
+            from_userId: userId,
+            to_userId: clickedUserId,
+            message: textArea
+        }
+
+        try {
+            await axios.post('http://localhost:8000/message', { message })
+            getUserMessages()
+            getClickedUsersMessages()
+            setTextArea("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <div className="chat-input">
             <textarea value={textArea} onChange={(e) => setTextArea(e.target.value)}/>
-            <button className="secondary-button">Submit</button>
+            <button className="secondary-button" onClick={addMessage}>Submit</button>
         </div>
+    )
+}
 
-)}
-
-export default ChatInput;
+export default ChatInput
