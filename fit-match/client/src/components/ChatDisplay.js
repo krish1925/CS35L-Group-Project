038d9482da7c6cab8b4490f601bcc08a2bc5766b
Sam Hopkins <samthehopkin@gmail.com>
2023-03-13 18:52:17 -0700

    update chat display

--- fit-match/client/src/components/ChatDisplay.js
+++ fit-match/client/src/components/ChatDisplay.js
@@ -1,40 +1,41 @@
-import Chat from "./Chat";
-import ChatInput from "./ChatInput";
+import Chat from './Chat'
+import ChatInput from './ChatInput'
 import axios from 'axios'
-import {useState, useEffect} from'react'
+import {useState, useEffect} from "react"
 
-const  ChatDisplay = ({user, clickedUser}) =>{
+
+const ChatDisplay = ({ user , clickedUser }) => {
     const userId = user?.user_id
-    const clickedUserId = clickedUserId?.user_id
+    const clickedUserId = clickedUser?.user_id
     const [usersMessages, setUsersMessages] = useState(null)
     const [clickedUsersMessages, setClickedUsersMessages] = useState(null)
 
     const getUsersMessages = async () => {
-        try {
-        const response = await axios.get('http://localhost:8000/messages', {
-            params: {userId: userId, correspondingUserId: clickedUserId}
-        })
-        setUsersMessages(response.data)
-    } catch(error) {
-        console.log(error)
+     try {
+            const response = await axios.get('http://localhost:8000/messages', {
+                params: { userId: userId, correspondingUserId: clickedUserId}
+            })
+         setUsersMessages(response.data)
+        } catch (error) {
+         console.log(error)
+     }
     }
-}
 
-const getClickedUsersMessages = async () => {
-    try {
-    const response = await axios.get('http://localhost:8000/messages', {
-        params: {userId: clickedUserId, correspondingUserId: userId}
-    })
-    setUsersMessages(response.data)
-} catch(error) {
-    console.log(error)
-}
-}
+    const getClickedUsersMessages = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/messages', {
+                params: { userId: clickedUserId , correspondingUserId: userId}
+            })
+            setClickedUsersMessages(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     useEffect(() => {
         getUsersMessages()
         getClickedUsersMessages()
-    }, [usersMessages, clickedUsersMessages])
+    }, [])
 
     const messages = []
 
@@ -55,17 +56,17 @@ const getClickedUsersMessages = async () => {
         formattedMessage['timestamp'] = message.timestamp
         messages.push(formattedMessage)
     })
-        
- 
-    const descendingOrderMessages = messages?.sort((a, b) => a.timestamp.localeCompare(b.timestamp))
-    console.log(usersMessages)
+
+    const descendingOrderMessages = messages?.sort((a,b) => a.timestamp.localeCompare(b.timestamp))
+
     return (
         <>
         <Chat descendingOrderMessages={descendingOrderMessages}/>
-        <ChatInput/>
+     <ChatInput
+         user={user}
+         clickedUser={clickedUser} getUserMessages={getUsersMessages} getClickedUsersMessages={getClickedUsersMessages}/>
         </>
-        
+    )
+}
 
-)}
-
-export default ChatDisplay;
+export default ChatDisplay
