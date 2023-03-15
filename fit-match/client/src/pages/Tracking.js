874ffccfde6b1237abbfda7d58627849bf2b73f1
Sam Hopkins <samthehopkin@gmail.com>
2023-03-14 18:03:05 -0700

    begin implementation of tracking page

--- fit-match/client/src/pages/Tracking.js
+++ fit-match/client/src/pages/Tracking.js
@@ -1,44 +1,131 @@
-import Nav from '../components/Nav' 
-import AuthModal from '../components/AuthModal'
-import {useState} from 'react'
-import './Home.css';
+import TinderCard from 'react-tinder-card'
+import { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie'
+import ChatContainer from '../components/ChatContainer'
+import axios from 'axios'
+import ViewProfile from './Viewprofile'
+import Leaderboard from './Leaderboard'
+import { useNavigate } from 'react-router-dom';
+import Nav from '../components/Nav'
 
-function Tracking(){
+function Tracking() {
+    const [user, setUser] = useState(null)
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+    const [genderedUsers, setGenderedUsers] = useState(null)
+    const [showProfile, setShowProfile] = useState(false);
+    const[error, setError] = useState(null)
+    const[email, setEmail] = useState(null)
+    const[total, setTotal] = useState(null)
+    const navigate = useNavigate();
 
-    const[showModal, setShowModal] = useState(false);
-    const[isSignUp, setIsSignUp] = useState(true);
+    const [formData, setFormData] = useState({
+        user_id: cookies.UserId,
+        workout_time: '',
+        workout_intensity: '',
+        favorite_exercise: '',
+        goals: ''
+      });
 
-    const authToken = false;
+    const userId = cookies.UserId
 
-    function handleClick() {
-        console.log('clicked');
-        setShowModal(true);
-        setIsSignUp(true);
+    const getUser = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/user', {
+                params: { userId }
+            })
+            setUser(response.data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    return (
-        <div className="overlay">
-            <Nav whitePage={false}
-                authToken={authToken}
-                setShowModal={setShowModal} 
-                showModal={showModal}
-                setIsSignUp={setIsSignUp}/>
-            <div className="home">
-                <h1 className="primary-title">FitMatch</h1>
-                <h2 className="tagline" style={{ color: '#ff4057', paddingTop: '0px', alignItems: 'top', fontFamily: 'Verdana, Geneva, Tahoma, sans-serif' }}> Match, Meet, Motivate </h2>
-                <button className="primary-button" onClick={handleClick}>
-                    {authToken ? 'Signout' : 'Create Account'}
-                </button>
+    const handleSubmit = async (e) => {}
 
-                {showModal && (
-                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
-                )}
+    function handleChange(e) {
+        const value = e.target.value;
+        const name = e.target.name;
+    
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    
+        if (name === "lift") {
+            if (value === "bench") {
+                setBench(true);
+            } else if (value === "deadlift") {
+                setDeadlift(true);
+            } else if (value === "squat") {
+                setSquat(true);
+            }
+        }
+    }
+    
+
+    const goLeaderboard = () => {
+        navigate('/leaderboard');
+    };
+
+    const [bench, setBench] = useState(false)
+    const [squat, setSquat] = useState(false)
+    const [deadlift, setDeadlift] = useState(false)
+
+
+
+    return (
+        <>
+    <Nav 
+                whitePage={true}
+                setShowModal={() => {}}
+                showModal={false}
+            />
+            <div className="onboarding">
+                <h2>TRACK YOUR PROGRESS</h2>
+
+                <form onSubmit={handleSubmit}>
+                 
+                   
+
+                   <section>
+                        <label>Lift</label>
+                        <div className="multiple-input-container">
+                            <input 
+                                id="bench"
+                                type="radio"
+                                name="lift"
+                                value="bench"
+                                onChange={handleChange}
+                                checked={bench}
+                            />
+                            <label htmlFor="bench">Bench</label>
+                            <input 
+                                id="squat"
+                                type="radio"
+                                name="lift"
+                                value="squat"
+                                onChange={handleChange}
+                                checked={squat}
+                            />
+                            <label htmlFor="squat">Squat</label>
+                            <input 
+                                id="deadlift"
+                                type="radio"
+                                name="lift"
+                                value="deadlift"
+                                onChange={handleChange}
+                                checked={deadlift}
+                            />
+                            <label htmlFor="deadlift">Deadlift</label>
+                        </div>   
+                        </section>
+
+                    
+
+                </form>
 
             </div>
-        </div>
-        
+        </>
     );
 }
 
 export default Tracking;
- 
